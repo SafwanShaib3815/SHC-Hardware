da94b4020ffcfd25935d21b45cbe3cd625f6c989
Saf <sh5safwan@hotmail.com>
2022-10-19 18:20:16 -0400

    database class added and RFID modified accordingly

--- RFID/smart_door.py
+++ RFID/smart_door.py
@@ -12,19 +12,19 @@ import rfid #script to read the rfid tags
 import behave #responce actions  script
 import blink #blinking led script
 #import temp_humid #temperature and humidity sensor script 
+import user_db
 
 pinsOut = [6,13,19,26] #creating a list (array) with the number of GPIO's that we use 
 led_out = 23
 stop_blink_thread=False
 GPIO.setwarnings(False)
 name=""
-ID=0
-dt=""
-path=""
-counter=0
-#initializing the firebase object
-myDB = firebase.FirebaseApplication("https://smarthomecontroller-a7978-default-rtdb.firebaseio.com/", None)
 
+#initializing the firebase object
+db = user_db.User_db("somemail@mail.com", "Admin1234#")#use this function to post to the realtime database db.data_send("Temperature", "Humidity", "Motion", "Smoke", "rfid ")
+
+
+        
 while True:
     ## Thread to to activate Temp-Humid sensor
     stop_blink_thread=False #allows the thread to go on
@@ -40,9 +40,6 @@ while True:
         print("\nMr. White is allowed to enter>>>>>>\n")
         behave.open_door() #opens the door
         name = "Mr White"
-        ID = id
-        dt=datetime.datetime.now()
-        path="/Door Records/Mr White"
         print ("****************************\n--Door closed Mr. White\n****************************")	
 
     #if blue fob taped
@@ -52,10 +49,7 @@ while True:
         blink_thread.join()
         behave.open_door() #oepns the door 
         name = "Mr Blue"
-        ID = id
-        dt=datetime.datetime.now()
-        path="/Door Records/Mr Blue"
-
+        
         print ("****************************\n--Door closed Mr. Blue\n****************************")
 
     
@@ -70,28 +64,9 @@ while True:
 
         #Values to be sent to DB
         name = "Unauthorized user attempt!"
-        ID = id
-        dt=datetime.datetime.now()
-        path="/Door Records/Unauthorized" #path of which data will be sent to in DB
+    db.data_send(None, None, None, None, name)
 
-    
-    command = False
-    #final object to be sent to DB
-    data= {
-        "name": name,
-        "ID" : ID, 
-        "Time": dt
-        }
-
-    #print(data)
-    
-    #path=path+str(counter)
-    rtrn=myDB.post(path,data) #send user name, card number and time to DB
-    #print(rtrn)
-    #getd = myDB.get('/Dummy Sensors/RFID/', None)
-    #print(getd)
-    #counter = counter+1
-
-if KeyboardInterrupt:
-    GPIO.cleanup()
+       
+    if KeyboardInterrupt:
+        GPIO.cleanup()
 GPIO.cleanup()
